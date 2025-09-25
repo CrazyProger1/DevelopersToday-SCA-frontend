6869e35ac9efd01ef089bbdb9916ebb2f1d4d9dd
CrazyProger1 <crazyproger1@gmail.com>
2025-09-25 19:31:38 +0300

    add: frontend readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-const Home = () => {
-  return <div></div>;
+import Link from "next/link";
+import { PAGES } from "@/config";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center">
+      <Button asChild>
+        <Link href={PAGES.dashboard}>Dashboard</Link>
+      </Button>
+    </div>
+  );
 };
 
-export default Home;
+export default Page;
